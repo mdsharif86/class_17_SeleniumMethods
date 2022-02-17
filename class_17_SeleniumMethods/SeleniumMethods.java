fbc21a87e500ab0fdda6645ccaee6d8ab0678f7b
tanvir <mdtanvirsharif@gmail.com>
2022-02-17 12:52:40 -0600

    SeleniumMethodAdded

---
+++ class_17_SeleniumMethods/SeleniumMethods.java
@@ -0,0 +1,14 @@
+
+public class SeleniumMethods {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Java\\Chromedrive\\chromedriver_win32\\chromedriver.exe";
+		
+		Webdriver site;
+		site = new ChromeDriver();
+		
+		
+
+	}
+
+}
